244a32dafd198c65d6ddb7df59764c6780513d36
Md Istiaq Alam <gollaboss@gmail.com>
2022-11-09 10:12:45 +1100

    ADD ThemeProvider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,19 @@ import "./App.css";
 import NavigationBar from "./components/NavigationBar";
 import Header from "./components/Header";
 import SignInSide from "./components/LoginForm";
+import { ThemeProvider } from '@material-ui/core';
+import theme from "./theme";
+
 function App() {
   return (
+      <ThemeProvider theme={theme}>
+
     <div className="App">
       <NavigationBar />
       <Header />
       <SignInSide />
     </div>
+      </ThemeProvider>
   );
 }
 
