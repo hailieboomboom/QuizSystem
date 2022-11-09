f60ab5c16b1fd0bfa8c58aea7f9dd227fa41f788
Bishar Ibrahim <bisharxibrahim@gmail.com>
2022-11-09 12:48:45 +1100

    added questions page to show all questions, and css for question card styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import Login from "./pages/Login";
 import Logout from "./pages/Logout";
 import CreateQuestion from "./pages/CreateQuestion";
 import CreateQuiz from "./pages/CreateQuiz"
+import Questions from "./pages/Questions"
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
                 <Route path="logout" element={<Logout/>} />
                 <Route path="register" element={<Register/>} />
                 <Route path="quiz" element={<Quiz/>} />
+                <Route path="questions" element={<Questions/>} />
             </Routes>
         </BrowserRouter>
 
