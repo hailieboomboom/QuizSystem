5db02607d4d28355021f1f4aa0f6bdb8ce36585a
Moshak67 <moshak67@gmail.com>
2022-11-09 10:53:12 +1000

    Mohammad pages and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,14 @@ import "./App.css";
 import NavigationBar from "./components/NavigationBar";
 import Header from "./components/Header";
 import SignInSide from "./components/LoginForm";
-import { ThemeProvider } from '@material-ui/core';
-import theme from "./theme";
 
 function App() {
   return (
-      <ThemeProvider theme={theme}>
-
     <div className="App">
       <NavigationBar />
       <Header />
       <SignInSide />
     </div>
-      </ThemeProvider>
   );
 }
 
