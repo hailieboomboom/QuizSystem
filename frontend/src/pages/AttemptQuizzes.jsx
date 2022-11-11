fde5c7de61204732018204ccd9b7e66f66dee56c
Md Istiaq Alam <gollaboss@gmail.com>
2022-11-11 12:26:55 +1100

    Added Filter option in View Quiz

--- frontend/src/pages/AttemptQuizzes.jsx
+++ frontend/src/pages/AttemptQuizzes.jsx
@@ -13,12 +13,25 @@ import { useRecoilState } from 'recoil';
 import { attemptQuizState } from '../recoil/Atoms'
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 import '../styles/QuizzesTableStyle.css';
+import TextField from "@mui/material/TextField";
 
 const AttemptQuizzes = () => {
 
     const [quizzes, setQuizzes] = React.useState([]);
     const [quiz, setQuiz] = useRecoilState(attemptQuizState);
+
+    const filterOptions = createFilterOptions({
+        matchFrom: 'start',
+        stringify: (option) => option.title,
+    });
+
+    const quizCategories = [
+        {title: "Interview"},
+        {title: "Skill"},
+        {title: "All"}
+    ];
 
     React.useEffect(() => {
         axios.get("https://the-trivia-api.com/api/questions?limit=10").then((response) => {
@@ -33,19 +46,42 @@ const AttemptQuizzes = () => {
 
 
     if (!quizzes) return null;
-
     return (
         <Grid
             container
             direction="column"
             justifyContent="flex-start"
             alignItems="center"
+            spacing={3}
         >
             <Grid item>
                 <Typography variant="h2" gutterBottom>
                     Available Quizzes
                 </Typography>
             </Grid>
+
+            <Grid
+                item
+                direction="row"
+                justifyContent="space-between"
+                alignItems="flex-start"
+            >
+                <Grid item />
+                <Grid item>
+                    <Autocomplete
+                        disableClearable
+                        id="filter-demo"
+                        options={quizCategories}
+                        getOptionLabel={(option) => option.title}
+                        filterOptions={filterOptions}
+                        sx={{ width: 300 }}
+                        renderInput={(params) => <TextField {...params} label="Filter" />}
+                    />
+                </Grid>
+
+
+            </Grid>
+
             <Grid item>
                 <TableContainer className={"table"} component={Paper} sx={{ width:700 }}>
                     <Table sx={{ minWidth: 650 }} aria-label="simple table">
