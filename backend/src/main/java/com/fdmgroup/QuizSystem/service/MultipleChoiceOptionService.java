c7b6404b7a7443addcd9a15eb1c7b98e9550ad28
Chris <christangsdy@gmail.com>
2022-11-13 07:34:55 +1100

    Finish mcq controller

--- backend/src/main/java/com/fdmgroup/QuizSystem/service/MultipleChoiceOptionService.java
+++ backend/src/main/java/com/fdmgroup/QuizSystem/service/MultipleChoiceOptionService.java
@@ -1,11 +1,19 @@
 package com.fdmgroup.QuizSystem.service;
 
+import com.fdmgroup.QuizSystem.dto.McqDto.CorrectOptionDto;
+import com.fdmgroup.QuizSystem.dto.McqDto.McqOptionDto;
+import com.fdmgroup.QuizSystem.exception.McqException.McqOptionNotValidException;
+import com.fdmgroup.QuizSystem.exception.McqException.NoDataFoundException;
+import com.fdmgroup.QuizSystem.model.MultipleChoiceQuestion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.fdmgroup.QuizSystem.model.MultipleChoiceOption;
 import com.fdmgroup.QuizSystem.repository.MultipleChoiceOptionRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -17,5 +25,98 @@ public class MultipleChoiceOptionService {
 	
 	public MultipleChoiceOption save(MultipleChoiceOption mco) {
 		return this.mcoRepo.save(mco);
+	}
+
+
+	public MultipleChoiceOption createMcqOption(McqOptionDto mcqOptionDto, MultipleChoiceQuestion mcqQuestion){
+		MultipleChoiceOption newOption = new MultipleChoiceOption();
+		newOption.setCorrect(mcqOptionDto.isCorrect());
+		newOption.setOptionDetail(mcqOptionDto.getOptionDescription());
+		newOption.setMcq(mcqQuestion);
+		return mcoRepo.save(newOption);
+	}
+
+	public List<MultipleChoiceOption> createListOfOption(List<McqOptionDto> mcqOptionDtoList, MultipleChoiceQuestion mcqQuestion){
+		List<MultipleChoiceOption> optionList = new ArrayList<>();
+		int numberOfCorrectOption = 0;
+
+		if (optionList.size() <= 1)
+			throw new McqOptionNotValidException("Please provide at least more than one option");
+
+		for(McqOptionDto mcqOption: mcqOptionDtoList) {
+			if (mcqOption.isCorrect())
+				numberOfCorrectOption++;
+			optionList.add(createMcqOption(mcqOption, mcqQuestion));
+		}
+		if (numberOfCorrectOption !=1)
+			throw new McqOptionNotValidException("Please only choose one correct option");
+
+
+
+
+		return optionList;
+
+	}
+
+	public McqOptionDto getMcqOptionDto(MultipleChoiceOption option){
+		McqOptionDto mcqOptionDto = new McqOptionDto();
+		mcqOptionDto.setOptionDescription(option.getOptionDetail());
+		mcqOptionDto.setCorrect(option.isCorrect());
+		return mcqOptionDto;
+
+	}
+
+	public List<McqOptionDto> listOptionsForMcq(long questionId){
+		List<MultipleChoiceOption> optionList =  this.getMcqOption(questionId);
+		List<McqOptionDto> optionDtoList = new ArrayList<>();
+		for(MultipleChoiceOption option:optionList){
+			optionDtoList.add(getMcqOptionDto(option));
+		}
+		return optionDtoList;
+	}
+
+	public List<MultipleChoiceOption> getMcqOption(Long id){
+		return mcoRepo.findAllByMcqId(id);
+	}
+
+
+	public List<MultipleChoiceOption> updateMcqOption(List<McqOptionDto> mcqOptionDtoList,long macId){
+
+		List<MultipleChoiceOption> originalMcqOption = mcoRepo.findAllByMcqId(macId);
+		if(originalMcqOption.size()!=mcqOptionDtoList.size()){
+			throw new McqOptionNotValidException("You can't change the number of options after creating it");
+		}
+		int i = 0;
+		for (MultipleChoiceOption option:originalMcqOption){
+			option.setCorrect(mcqOptionDtoList.get(i).isCorrect());
+			option.setOptionDetail(mcqOptionDtoList.get(i).getOptionDescription());
+			i++;
+		}
+		return originalMcqOption;
+	}
+
+	public void deleteQuestionOptions(long questionId){
+		List<MultipleChoiceOption> options = getMcqOption(questionId);
+
+		if(options == null) throw new NoDataFoundException("Options Not Founded for this question");
+
+		for (MultipleChoiceOption option:options)
+			mcoRepo.delete(option);
+
+	}
+
+	public CorrectOptionDto getRightOption(long questionId){
+		List<MultipleChoiceOption> options = getMcqOption(questionId).stream().filter(option -> option.isCorrect()).toList();
+		if(options.size() != 1)
+			throw new McqOptionNotValidException("Multiple Choice Question Not Found");
+
+		MultipleChoiceOption option = options.get(0);
+		CorrectOptionDto correctOptionDto = new CorrectOptionDto();
+		correctOptionDto.setOptionId(option.getId());
+		correctOptionDto.setOptionDescription(option.getOptionDetail());
+		correctOptionDto.setCorrect(true);
+
+		return correctOptionDto;
+
 	}
 }
