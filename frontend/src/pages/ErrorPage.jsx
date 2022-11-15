2b40b24c56535bffd9cddfa638abfa00267acf4a
Bishar Ibrahim <bisharxibrahim@gmail.com>
2022-11-15 16:43:12 +1100

    home page design

---
+++ frontend/src/pages/ErrorPage.jsx
@@ -0,0 +1,19 @@
+import Grid from "@mui/material/Grid";
+import {Stack} from "@mui/material";
+import Box from "@mui/material/Box";
+import React from "react";
+
+const ErrorPage = () => {
+
+    function getErrorMessage(props){
+
+    }
+
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+
+    )
+}
+export default ErrorPage
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import '../styles/profileStyle.css'
+import editButton from '../styles/editButton.png'
+import profilePicture from '../styles/profilePicture.png'
 import Grid from "@mui/material/Grid";
+
+
+
 
 const Profile = () => {
   return (
-      <div className={"profileDiv"}>
-        <Grid className={"profileGrid"}>
-          <Grid item>
-            <div className={"profileCenter"}>
-              <h1 className={"profileTitle"}>This is your profile!</h1>
-            </div>
-          </Grid>
-        </Grid>
+      <div className={"profileContainer"}>
+          <div className={"profileBox"}>
+              <img src={profilePicture}/>
+              <img src={editButton}/>
+              <h3>Name</h3>
+              <p>Welcome to your personal page!</p>
+          </div>
+
+        {/*<Grid className={"profileGrid"}>*/}
+        {/*  <Grid item>*/}
+        {/*    <div className={"profileCenter"}>*/}
+        {/*      <h1 className={"profileTitle"}>This is your profile!</h1>*/}
+        {/*    </div>*/}
+        {/*  </Grid>*/}
+        {/*</Grid>*/}
       </div>
+
   )
 }
 
