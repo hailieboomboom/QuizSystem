bc69bd7c421be2d601b9b5605bf09baba394e17d
Bishar Ibrahim <bisharxibrahim@gmail.com>
2022-11-15 11:44:10 +1100

    added edit question functionality for the question part

--- frontend/src/pages/EditQuestion.jsx
+++ frontend/src/pages/EditQuestion.jsx
@@ -10,23 +10,81 @@ import { Container } from '@mui/material';
 import { Link } from "react-router-dom";
 import {useRecoilState} from "recoil";
 import {editQuestionState} from "../recoil/Atoms";
+import axios from "axios";
+const API_URL = 'http://localhost:8088/QuizSystem/api/questions/mcqs'
 
 const EditQuestion = () => {
     const [editQuestions, setEditQuestions] = useRecoilState(editQuestionState)
 
     const [question, setQuestion] = useState('');
 
-    function handleSaveQuestion() {
-        const url = "http://localhost:8088/QuizSystem/api/questions/mcqs";
+    const [answer, setAnswer] = useState('');
+
+    const url = `${API_URL}/${editQuestions.questionId}`
+
+    const counter = 0;
+
+    const handleSubmit = event => {
+        console.log('handleSubmit ran');
+        event.preventDefault(); // 👈️ prevent page refresh
+
+    };
+
+    function handleSaveQuestion(){
+        console.log(question)
+        const data = {
+
+            "options": [
+            {
+                "correct": true,
+                "optionDescription": answer
+            },
+            {
+                "correct": false,
+                "optionDescription": answer
+            },
+            {
+                "correct": false,
+                "optionDescription": answer
+            }
+        ],
+            "questionDetails": question,
+            "tags": [
+            "interview"
+        ],
+            "userId": 0
+
+        }
+        axios.put(url, data)
+            .then(data => {
+                console.log(data);
+            })
+            .catch((err) =>{
+                console.log(err)
+            });
     }
+
+
 
     return (
         <React.Fragment>
             <Container>
                 <Typography variant="h6" gutterBottom>
-                    Edit Question: {editQuestions.questionDetail}
+                    Edit Question: {editQuestions.questionDetail} {editQuestions.questionId}
                 </Typography>
+
                 <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <FormControl variant="standard"  fullWidth>
+                            <TextField
+                                id="questionId"
+                                name="questionString"
+                                value={"Question ID: " + editQuestions.questionId}
+                                fullWidth
+                                variant="standard"
+                            />
+                        </FormControl>
+                    </Grid>
                     <Grid item xs={12}>
                         <FormControl variant="standard"  fullWidth>
                             <TextField
@@ -36,6 +94,7 @@ const EditQuestion = () => {
                                 label="Question"
                                 fullWidth
                                 variant="standard"
+                                onChange={event => setQuestion(event.target.value)}
                             />
                         </FormControl>
                     </Grid>
@@ -44,11 +103,12 @@ const EditQuestion = () => {
                             {editQuestions.mcqOptionDtoList.map((option) => (
                                 <TextField
                                     required
-                                    id="Answer"
+                                    id={id}
                                     name="questionString"
                                     fullWidth
                                     variant="standard"
                                     defaultValue={option.optionDescription}
+                                    onChange={event => setAnswer(event.target.value)}
                                 > {option.optionDescription} </TextField>
                             ))}
 
@@ -58,8 +118,8 @@ const EditQuestion = () => {
                 </Grid>
 
                     <Grid item xs={1}>
-
-                        <Button  onClick={handleSaveQuestion} variant="outlined" as={Link} to="/successEditQuestion">
+                        {/*as={Link} to="/successEditQuestion"*/}
+                        <Button  onClick={handleSaveQuestion} variant="outlined" >
                             Edit Question
                         </Button>
                     </Grid>
